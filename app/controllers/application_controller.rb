47ddc188302152fa2773ce9ccd69b0814917d24b
EC2 Default User <ec2-user@ip-172-31-28-195.ap-northeast-1.compute.internal>
2021-06-05 07:06:07 +0000

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    if current_user
+      flash[:notice] = "ログインに成功しました"
+      posts_path
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
