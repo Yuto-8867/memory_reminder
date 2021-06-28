7a64b4c9d0141fc106206bfd6df54db1f90b9f25
Yuto Inoue <baseball.taki.yi.1994@gmail.com>
2021-06-28 12:15:32 +0000

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,11 @@
 // about supported directives.
 //
 //= require jquery3
+//= require rails-ujs
 //= require popper
 //= require bootstrap-sprockets
 
-//= require rails-ujs
+
 //= require turbolinks
 //= require_tree .
 
